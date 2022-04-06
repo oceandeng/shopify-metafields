6e71bda65d15d71fc5625048df978eac1cb25166
ocean_deng <denghaiyang@300.cn>
2022-04-06 14:19:40 +0800

    up
    Signed-off-by: ocean_deng <denghaiyang@300.cn>

--- craco.config.js
+++ craco.config.js
@@ -9,7 +9,7 @@ module.exports = {
         configure: (webpackConfig, { env, paths }) => {
             webpackConfig.output = {
                 ...webpackConfig.output,
-                publicPath: "./",
+                publicPath: "/shopify-metafields",
             };
             return webpackConfig;
         },
